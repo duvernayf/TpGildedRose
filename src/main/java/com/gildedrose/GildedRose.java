570b1e317d171de21f29673159c2a4e18159746e
François Duvernay <francois.duvernay@hes.adhes.hesge.ch>
2017-05-04 10:45:12 +0200

    Deuxième amelioration GildedRose

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -17,24 +17,16 @@ class GildedRose {
             int sellIn = item.sellIn;
             
             if (!name.equals("Aged Brie") && !name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (quality > 0) {
-                    if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                        quality--;
-                    }
+                if ((quality > 0) && (!name.equals("Sulfuras, Hand of Ragnaros"))) {
+                    quality--;
                 }
             } else {
                 if (quality < 50) {
                     quality++;
-                    if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (sellIn < 11) {
-                            if (quality < 50) {
-                                quality++;
-                            }
-                        }
-                        if (sellIn < 6) {
-                            if (quality < 50) {
-                                quality++;
-                            }
+                    if (name.equals("Backstage passes to a TAFKAL80ETC concert") && ((sellIn < 11) && (quality < 50))) {
+                        quality++;
+                        if ((sellIn < 6) && (quality < 50)) {
+                            quality++;
                         }
                     }
                 }
@@ -45,18 +37,14 @@ class GildedRose {
             if (sellIn < 0) {
                 if (!name.equals("Aged Brie")) {
                     if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (quality > 0) {
-                            if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                                quality--;
-                            }
+                        if (quality > 0 && (!name.equals("Sulfuras, Hand of Ragnaros"))) {
+                            quality--;
                         }
                     } else {
                         quality = 0;
                     }
-                } else {
-                    if (quality < 50) {
-                        quality++;
-                    }
+                } else if (quality < 50){
+                    quality++;
                 }
             }
             item.quality = quality;
