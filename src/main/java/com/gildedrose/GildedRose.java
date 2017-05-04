ba997fe98a02d349d17a846db7f948e4d509fc48
François Duvernay <francois.duvernay@hes.adhes.hesge.ch>
2017-05-04 10:09:52 +0200

    Première amélioration GildedRose

---
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -0,0 +1,66 @@
+package com.gildedrose;
+
+import javafx.scene.CacheHint;
+
+class GildedRose {
+
+    Item[] items;
+
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
+        for (Item item : items) {
+            String name = item.name;
+            int quality = item.quality;
+            int sellIn = item.sellIn;
+            
+            if (!name.equals("Aged Brie") && !name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (quality > 0) {
+                    if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                        quality--;
+                    }
+                }
+            } else {
+                if (quality < 50) {
+                    quality++;
+                    if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (sellIn < 11) {
+                            if (quality < 50) {
+                                quality++;
+                            }
+                        }
+                        if (sellIn < 6) {
+                            if (quality < 50) {
+                                quality++;
+                            }
+                        }
+                    }
+                }
+            }
+            if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                sellIn--;
+            }
+            if (sellIn < 0) {
+                if (!name.equals("Aged Brie")) {
+                    if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (quality > 0) {
+                            if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                                quality--;
+                            }
+                        }
+                    } else {
+                        quality = 0;
+                    }
+                } else {
+                    if (quality < 50) {
+                        quality++;
+                    }
+                }
+            }
+            item.quality = quality;
+            item.sellIn = sellIn;
+        }
+    }
+}
